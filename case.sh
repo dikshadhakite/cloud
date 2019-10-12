0170a55164ffa1cfd5ba4b98290c629a703f1817
dikshadhakite <36851806+dikshadhakite@users.noreply.github.com>
2019-10-12 17:46:20 +0530

    Add files via upload

---
+++ case.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter your number"
+read n 
+case $n in
+1)
+echo "monday" ;;
+2)
+echo "tuesday" ;;
+3)
+echo "wednesday";;
+4)
+echo "thursday";;
+;)
+echo "sorryyyy,no working day..!!!" ;;
+esac
